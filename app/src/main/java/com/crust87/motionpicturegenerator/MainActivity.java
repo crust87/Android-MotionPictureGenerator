e3cece8dde0092aee9fa75a54e95daff2305443d
mabi <crust87@gmail.com>
2015-12-29 14:06:12 +0900

    add eco player

--- app/src/main/java/com/crust87/motionpicturegenerator/MainActivity.java
+++ app/src/main/java/com/crust87/motionpicturegenerator/MainActivity.java
@@ -1,13 +1,381 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.crust87.motionpicturegenerator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest.permission;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.crust87.motionpicturegenerator.player.ExoVideoPlayer;
+import com.crust87.motionpicturegenerator.player.ExtractorRendererBuilder;
+import com.google.android.exoplayer.AspectRatioFrameLayout;
+import com.google.android.exoplayer.ExoPlaybackException;
+import com.google.android.exoplayer.MediaCodecTrackRenderer.DecoderInitializationException;
+import com.google.android.exoplayer.MediaCodecUtil.DecoderQueryException;
+import com.google.android.exoplayer.MediaFormat;
+import com.google.android.exoplayer.audio.AudioCapabilities;
+import com.google.android.exoplayer.audio.AudioCapabilitiesReceiver;
+import com.google.android.exoplayer.drm.UnsupportedDrmException;
+import com.google.android.exoplayer.metadata.GeobMetadata;
+import com.google.android.exoplayer.metadata.PrivMetadata;
+import com.google.android.exoplayer.metadata.TxxxMetadata;
+import com.google.android.exoplayer.util.MimeTypes;
+import com.google.android.exoplayer.util.Util;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * An activity that plays media using {@link ExoVideoPlayer}.
+ */
+public class MainActivity extends Activity implements SurfaceHolder.Callback,
+        ExoVideoPlayer.Listener, ExoVideoPlayer.Id3MetadataListener,
+        AudioCapabilitiesReceiver.Listener {
+
+    private static final String TAG = "MainActivity";
+    private static final int MENU_GROUP_TRACKS = 1;
+    private static final int ID_OFFSET = 2;
+
+    private EventLogger eventLogger;
+    private AspectRatioFrameLayout videoFrame;
+    private SurfaceView surfaceView;
+
+    private ExoVideoPlayer player;
+    private boolean playerNeedsPrepare;
+
+    private long playerPosition;
+
+    private Uri contentUri;
+
+    private AudioCapabilitiesReceiver audioCapabilitiesReceiver;
+
+    // Activity lifecycle
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.player_activity);
+
+        videoFrame = (AspectRatioFrameLayout) findViewById(R.id.video_frame);
+        surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        surfaceView.getHolder().addCallback(this);
+
+        audioCapabilitiesReceiver = new AudioCapabilitiesReceiver(this, this);
+        audioCapabilitiesReceiver.register();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            Uri selectedVideoUri = data.getData();
+            Intent intent = new Intent(this, MainActivity.class).setData(selectedVideoUri);
+            startActivity(intent);
+        }
+    }
+
+    public void onLoadButtonClicked(View view) {
+        Intent lIntent = new Intent(Intent.ACTION_PICK);
+        lIntent.setType("video/*");
+        lIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivityForResult(lIntent, 1000);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        releasePlayer();
+        playerPosition = 0;
+        setIntent(intent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        contentUri = intent.getData();
+        if (player == null) {
+            if (!maybeRequestPermission()) {
+                preparePlayer(true);
+            }
+        } else {
+            player.setBackgrounded(false);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        releasePlayer();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        audioCapabilitiesReceiver.unregister();
+        releasePlayer();
+    }
+
+    // AudioCapabilitiesReceiver.Listener methods
+
+    @Override
+    public void onAudioCapabilitiesChanged(AudioCapabilities audioCapabilities) {
+        if (player == null) {
+            return;
+        }
+        boolean backgrounded = player.getBackgrounded();
+        boolean playWhenReady = player.getPlayWhenReady();
+        releasePlayer();
+        preparePlayer(playWhenReady);
+        player.setBackgrounded(backgrounded);
+    }
+
+    // Permission request listener method
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            preparePlayer(true);
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.storage_permission_denied,
+                    Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    // Permission management methods
+
+    /**
+     * Checks whether it is necessary to ask for permission to read storage. If necessary, it also
+     * requests permission.
+     *
+     * @return true if a permission request is made. False if it is not necessary.
+     */
+    @TargetApi(23)
+    private boolean maybeRequestPermission() {
+        if (requiresPermission(contentUri)) {
+            requestPermissions(new String[]{permission.READ_EXTERNAL_STORAGE}, 0);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @TargetApi(23)
+    private boolean requiresPermission(Uri uri) {
+        return Util.SDK_INT >= 23
+                && Util.isLocalFileUri(uri)
+                && checkSelfPermission(permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED;
+    }
+
+    // Internal methods
+    private ExoVideoPlayer.RendererBuilder getRendererBuilder() {
+        String userAgent = Util.getUserAgent(this, "MotionPictureGenerator");
+        return new ExtractorRendererBuilder(this, userAgent, contentUri);
+    }
+
+    private void preparePlayer(boolean playWhenReady) {
+        if (player == null) {
+            player = new ExoVideoPlayer(getRendererBuilder());
+            player.addListener(this);
+            player.setMetadataListener(this);
+            player.seekTo(playerPosition);
+            playerNeedsPrepare = true;
+            eventLogger = new EventLogger();
+            eventLogger.startSession();
+            player.addListener(eventLogger);
+            player.setInfoListener(eventLogger);
+            player.setInternalErrorListener(eventLogger);
+        }
+        if (playerNeedsPrepare) {
+            player.prepare();
+            playerNeedsPrepare = false;
+        }
+        player.setSurface(surfaceView.getHolder().getSurface());
+        player.setPlayWhenReady(playWhenReady);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playerPosition = player.getCurrentPosition();
+            player.release();
+            player = null;
+            eventLogger.endSession();
+            eventLogger = null;
+        }
+    }
+
+    // ExoVideoPlayer.Listener implementation
+
+    @Override
+    public void onStateChanged(boolean playWhenReady, int playbackState) {
+        if (playbackState == com.google.android.exoplayer.ExoPlayer.STATE_ENDED) {
+        }
+    }
+
+    @Override
+    public void onError(Exception e) {
+        String errorString = null;
+        if (e instanceof UnsupportedDrmException) {
+            // Special case DRM failures.
+            UnsupportedDrmException unsupportedDrmException = (UnsupportedDrmException) e;
+            errorString = getString(Util.SDK_INT < 18 ? R.string.error_drm_not_supported
+                    : unsupportedDrmException.reason == UnsupportedDrmException.REASON_UNSUPPORTED_SCHEME
+                    ? R.string.error_drm_unsupported_scheme : R.string.error_drm_unknown);
+        } else if (e instanceof ExoPlaybackException
+                && e.getCause() instanceof DecoderInitializationException) {
+            // Special case for decoder initialization failures.
+            DecoderInitializationException decoderInitializationException =
+                    (DecoderInitializationException) e.getCause();
+            if (decoderInitializationException.decoderName == null) {
+                if (decoderInitializationException.getCause() instanceof DecoderQueryException) {
+                    errorString = getString(R.string.error_querying_decoders);
+                } else if (decoderInitializationException.secureDecoderRequired) {
+                    errorString = getString(R.string.error_no_secure_decoder,
+                            decoderInitializationException.mimeType);
+                } else {
+                    errorString = getString(R.string.error_no_decoder,
+                            decoderInitializationException.mimeType);
+                }
+            } else {
+                errorString = getString(R.string.error_instantiating_decoder,
+                        decoderInitializationException.decoderName);
+            }
+        }
+        if (errorString != null) {
+            Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_LONG).show();
+        }
+        playerNeedsPrepare = true;
+    }
+
+    @Override
+    public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees,
+                                   float pixelWidthAspectRatio) {
+        videoFrame.setAspectRatio(
+                height == 0 ? 1 : (width * pixelWidthAspectRatio) / height);
+    }
+
+    private static String buildTrackName(MediaFormat format) {
+        if (format.adaptive) {
+            return "auto";
+        }
+        String trackName;
+        if (MimeTypes.isVideo(format.mimeType)) {
+            trackName = joinWithSeparator(joinWithSeparator(buildResolutionString(format),
+                    buildBitrateString(format)), buildTrackIdString(format));
+        } else if (MimeTypes.isAudio(format.mimeType)) {
+            trackName = joinWithSeparator(joinWithSeparator(joinWithSeparator(buildLanguageString(format),
+                    buildAudioPropertyString(format)), buildBitrateString(format)),
+                    buildTrackIdString(format));
+        } else {
+            trackName = joinWithSeparator(joinWithSeparator(buildLanguageString(format),
+                    buildBitrateString(format)), buildTrackIdString(format));
+        }
+        return trackName.length() == 0 ? "unknown" : trackName;
+    }
+
+    private static String buildResolutionString(MediaFormat format) {
+        return format.width == MediaFormat.NO_VALUE || format.height == MediaFormat.NO_VALUE
+                ? "" : format.width + "x" + format.height;
+    }
+
+    private static String buildAudioPropertyString(MediaFormat format) {
+        return format.channelCount == MediaFormat.NO_VALUE || format.sampleRate == MediaFormat.NO_VALUE
+                ? "" : format.channelCount + "ch, " + format.sampleRate + "Hz";
+    }
+
+    private static String buildLanguageString(MediaFormat format) {
+        return TextUtils.isEmpty(format.language) || "und".equals(format.language) ? ""
+                : format.language;
+    }
+
+    private static String buildBitrateString(MediaFormat format) {
+        return format.bitrate == MediaFormat.NO_VALUE ? ""
+                : String.format(Locale.US, "%.2fMbit", format.bitrate / 1000000f);
+    }
+
+    private static String joinWithSeparator(String first, String second) {
+        return first.length() == 0 ? second : (second.length() == 0 ? first : first + ", " + second);
+    }
+
+    private static String buildTrackIdString(MediaFormat format) {
+        return format.trackId == null ? "" : " (" + format.trackId + ")";
+    }
+
+    private boolean onTrackItemClick(MenuItem item, int type) {
+        if (player == null || item.getGroupId() != MENU_GROUP_TRACKS) {
+            return false;
+        }
+        player.setSelectedTrack(type, item.getItemId() - ID_OFFSET);
+        return true;
+    }
+
+    // ExoVideoPlayer.MetadataListener implementation
+    @Override
+    public void onId3Metadata(Map<String, Object> metadata) {
+        for (Map.Entry<String, Object> entry : metadata.entrySet()) {
+            if (TxxxMetadata.TYPE.equals(entry.getKey())) {
+                TxxxMetadata txxxMetadata = (TxxxMetadata) entry.getValue();
+                Log.i(TAG, String.format("ID3 TimedMetadata %s: description=%s, value=%s",
+                        TxxxMetadata.TYPE, txxxMetadata.description, txxxMetadata.value));
+            } else if (PrivMetadata.TYPE.equals(entry.getKey())) {
+                PrivMetadata privMetadata = (PrivMetadata) entry.getValue();
+                Log.i(TAG, String.format("ID3 TimedMetadata %s: owner=%s",
+                        PrivMetadata.TYPE, privMetadata.owner));
+            } else if (GeobMetadata.TYPE.equals(entry.getKey())) {
+                GeobMetadata geobMetadata = (GeobMetadata) entry.getValue();
+                Log.i(TAG, String.format("ID3 TimedMetadata %s: mimeType=%s, filename=%s, description=%s",
+                        GeobMetadata.TYPE, geobMetadata.mimeType, geobMetadata.filename,
+                        geobMetadata.description));
+            } else {
+                Log.i(TAG, String.format("ID3 TimedMetadata %s", entry.getKey()));
+            }
+        }
+    }
+
+    // SurfaceHolder.Callback implementation
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (player != null) {
+            player.setSurface(holder.getSurface());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Do nothing.
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (player != null) {
+            player.blockingClearSurface();
+        }
     }
 }
